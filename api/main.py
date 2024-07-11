900aa48f8493b0eadd55decee61427da028edce4
daniertg <febriandani00@gmail.com>
2024-07-11 14:27:03 +0700

    vc1

--- api/main.py
+++ api/main.py
@@ -1,16 +1,21 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from ..static import electre_four as ef
-from ..static.caesar_cipher import caesar_encode, caesar_decode
 from flask import Flask, request, render_template, redirect, url_for, send_file, jsonify
 from PIL import Image
-import os
 import json
 import pandas as pd
 import numpy as np
-app = Flask(__name__)
+from static import electre_four as ef
+from static.caesar_cipher import caesar_encode, caesar_decode
+
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
 @app.route('/')
 def home():
     return render_template("index.html")
+
 @app.route('/png_to_jpg', methods=["GET", "POST"])
 def png_to_jpg():
     converted = False
@@ -19,19 +24,20 @@ def png_to_jpg():
         if file and file.filename != "":
             img = Image.open(file)
             img = img.convert("RGB")  # Convert to RGB format
-            img.save("static/images/output.jpg", "JPEG")
+            img.save(os.path.join(app.static_folder, "images/output.jpg"), "JPEG")
             converted = True
     return render_template('png_to_jpg.html', converted=converted)
+
 @app.route('/download_image')
 def download_image():
-    return send_file('static/images/output.jpg', as_attachment=True)
+    return send_file(os.path.join(app.static_folder, 'images/output.jpg'), as_attachment=True)
     
 @app.route('/electre_four')
 def electre_four():
     return render_template("electre_four.html")
+
 @app.route('/post_electre_four', methods=["POST"])
 def post_electre_four():
-    
     matrix = request.form.get('matrix')
     weight = request.form.get('weight')
     
@@ -41,9 +47,9 @@ def post_electre_four():
     result = ef.initiation(matrix, weight)
     
     return jsonify({'message': 'success', 'result': result})
+
 @app.route('/post_csv_electre', methods=["POST"])
 def post_csv_electre():
-        
     csv_file = request.files['csv_file']
     data = pd.read_csv(csv_file, header=None)
     
@@ -53,6 +59,7 @@ def post_csv_electre():
         for i in range(len(data)):
             data_split = str(data.iloc[i].values).replace('[', '').replace(']', '').strip("'").split(';')
             data_temp.append(data_split)
+
         data = pd.DataFrame(data_temp)
     
     if type(data[0][0]) == str:
@@ -63,20 +70,24 @@ def post_csv_electre():
     result = data_list
     
     return jsonify({'message': 'success', 'result': result})
+
 @app.route('/Caesar_Cipher')
 def view_caesar_cipher():
     return render_template("caesar_cipher.html")
+
 @app.route('/Caesar_Cipher/result', methods=['POST'])
 def result():
     if request.method == 'POST':
         text = request.form['inputText']
         shift = int(request.form['shiftAmount'])
         operation = request.form['operation']
+
         result_text = ''
         if operation == 'encode':
             result_text = caesar_encode(text, shift)
         elif operation == 'decode':
             result_text = caesar_decode(text, shift)
+
         return render_template('caesar_cipher.html', result=result_text)  
     
 @app.route('/borda')
@@ -111,13 +122,11 @@ def post_borda():
 
     # Lakukan perhitungan Borda dengan fungsi calculate_borda_score
     data_to_process = calculate_borda_score(alternatif_names, frequencies, bobot_peringkat)
-
+    
     # Ubah data_to_process ke format yang diharapkan oleh JavaScript
     result = [{'Alternatif Name': item['Alternatif Name'], 'Skor': item['Skor']} for item in data_to_process]
 
     return jsonify({'message': 'success', 'result': result})
-
-
 
 @app.route('/copeland')
 def view_copeland():
